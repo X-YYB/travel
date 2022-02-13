6a8550616279206c9140d5b90d9f3181eedd52da
X-Ganen <1763108660@qq.com>
2022-02-13 21:57:16 +0800

    jia gou yan bian

--- x-ganen-web/src/main/java/com/young/service/BannerService.java
+++ x-ganen-web/src/main/java/com/young/service/BannerService.java
@@ -1,10 +1,24 @@
 package com.young.service;
 
+import com.young.pojo.Banner;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
 /**
  * @Description: TODO
  * @Author: yangyb
  * @Date:2022/2/13 21:23
  * Version: 1.0
  **/
+@Service
 public class BannerService {
+
+    /*
+    * 查询轮播图
+    * */
+
+    public List<Banner> findBanners(){
+        return null;
+    }
 }
